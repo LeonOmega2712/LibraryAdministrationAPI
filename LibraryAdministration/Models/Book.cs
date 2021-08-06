65398887e1ee8b08bb1c0a9063c0ce0fd194111f
Jonathan Muñoz <jonathan2712emr@gmail.com>
2021-08-06 17:22:08 -0500

    Agregar archivos de proyecto.

---
+++ LibraryAdministration/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryAdministration.Models
+{
+    public class Book
+    {
+        public string Title;
+        public DateTime YearPublished;
+        public int PageQuantity;
+        public int StockQuantity;
+        public int IdAuthor;
+    }
+}
