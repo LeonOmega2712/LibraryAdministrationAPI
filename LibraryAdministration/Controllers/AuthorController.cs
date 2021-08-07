fb63a239fbcef5a0355f230fc1b78b36505ca56a
Jonathan Muñoz <jonathan2712emr@gmail.com>
2021-08-07 00:48:08 -0500

    Added Get by id API function; Added Put example code.
    The example functionality and DbLess functions for Get by id API ins done, must finish DbLess functions of Put API.

--- LibraryAdministration/Controllers/AuthorController.cs
+++ LibraryAdministration/Controllers/AuthorController.cs
@@ -1,6 +1,8 @@
 ﻿using LibraryAdministration.Context;
 using LibraryAdministration.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,7 +17,7 @@ namespace LibraryAdministration.Controllers
     public class AuthorController : ControllerBase
     {
         #region AppDbContext initiation for DB usage
-        // When a DB is used...
+        // When a DB is used this code must be used...
         private readonly AppDbContext context;
         public AuthorController(AppDbContext context)
         {
@@ -25,12 +27,12 @@ namespace LibraryAdministration.Controllers
 
         #region Example list creation
         // For showing purposes the next list will be used as an example:
-        private List<Author> GenerateAuthorsList(uint quantity)
+        private static List<Author> GenerateAuthorsList(uint quantity)
         {
-            List<Author> Authors = new List<Author>();
+            List<Author> Authors = new();
             for (uint i = 1; i <= quantity; i++)
             {
-                Author Author = new Author()
+                Author Author = new()
                 {
                     Id = i,
                     Name = $"Author {i}",
@@ -44,40 +46,73 @@ namespace LibraryAdministration.Controllers
         }
         #endregion
 
-
+        #region API Methods
         // GET: api/<AuthorController>
         [HttpGet]
         public IEnumerable<Author> Get()
         {
-            /*When a DB is used...
-            return context.Authors.ToList();*/
+            /* When a DB is used...
+            * return context.Authors.ToList();
+            */
 
             return GenerateAuthorsList(3);
         }
 
         // GET api/<AuthorController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public Author Get(uint id)
         {
-            return "value";
+            /* When a DB is used...
+            *  var author = context.Authors.FirstOrDefault(_author => _author.Id == id);
+            *  return author;
+            */
+            List<Author> author = GenerateAuthorsList(id + 3);
+            Console.WriteLine($"Authors list has {author.Count} elements");
+            Console.WriteLine($"Author found by id {id}: {author.Find(author => author.Id == id).Name}");
+            return author.Find(author => author.Id == id);
         }
 
         // POST api/<AuthorController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult Post([FromBody] Author author)
         {
+            context.Authors.Add(author);
+            return Ok($"{author.Name}");
         }
 
         // PUT api/<AuthorController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public IActionResult Put(uint id, [FromBody] Author author)
         {
+            try
+            {
+                if (id != author.Id)
+                {
+                    return BadRequest();
+                }
+
+                var authorToUpdate = context.Authors.FirstOrDefault(author => author.Id == id);
+
+                if (authorToUpdate == null)
+                {
+                    return NotFound($"Author with id {id} not found");
+                }
+
+                authorToUpdate = author;
+                context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                throw;
+            }
+            return Ok();
         }
 
         // DELETE api/<AuthorController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public void Delete(uint id)
         {
         }
+        #endregion
     }
 }
