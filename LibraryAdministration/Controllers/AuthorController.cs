0f72045772654b008643482b55da2e70e31bcd7f
Jonathan Muñoz <jonathan2712emr@gmail.com>
2021-08-07 13:34:19 -0500

    API methods Put and Delete finished
    Changed the DB functions from PUT and DELETE methods to use example lists.

--- LibraryAdministration/Controllers/AuthorController.cs
+++ LibraryAdministration/Controllers/AuthorController.cs
@@ -52,8 +52,8 @@ namespace LibraryAdministration.Controllers
         public IEnumerable<Author> Get()
         {
             /* When a DB is used...
-            * return context.Authors.ToList();
-            */
+             * return context.Authors.ToList();
+             */
 
             return GenerateAuthorsList(3);
         }
@@ -63,9 +63,9 @@ namespace LibraryAdministration.Controllers
         public Author Get(uint id)
         {
             /* When a DB is used...
-            *  var author = context.Authors.FirstOrDefault(_author => _author.Id == id);
-            *  return author;
-            */
+             * var author = context.Authors.FirstOrDefault(_author => _author.Id == id);
+             * return author;
+             */
             List<Author> author = GenerateAuthorsList(id + 3);
             Console.WriteLine($"Authors list has {author.Count} elements");
             Console.WriteLine($"Author found by id {id}: {author.Find(author => author.Id == id).Name}");
@@ -76,55 +76,71 @@ namespace LibraryAdministration.Controllers
         [HttpPost]
         public IActionResult Post([FromBody] Author author)
         {
-            /* try
-            *  {
-            *     context.Authors.Add(author);
-            *     context.SaveChanges();
-            *     return Ok($"{author.Name}");
-            *  }
-            *  catch (Exception ex)
-            *  {
-            *     return BadRequest();
-            *  }
-            */
+            /* When DB is used...
+             * try
+             *  {
+             *     context.Authors.Add(author);
+             *     context.SaveChanges();
+             *     return Ok($"{author.Name}");
+             *  }
+             *  catch (Exception ex)
+             *  {
+             *      return BadRequest();
+             *  }
+             */
 
             List<Author> authors = GenerateAuthorsList(5);
             authors.Add(author);
-            return Ok($"El usuario insertado es {authors.Find(authors => authors.Id == author.Id).Name}");
-
+            return Ok($"El usuario insertado es {authors.Find(authors => authors.Name == author.Name).Name}");
         }
 
         // PUT api/<AuthorController>/5
         [HttpPut("{id}")]
         public IActionResult Put(uint id, [FromBody] Author author)
         {
-            if (author.Id == id)
-            {
-                context.Entry(author).State = EntityState.Modified;
-                context.SaveChanges();
-                return Ok();
-            }
-            else
-            {
-                return BadRequest();
-            }
+            /* When DB is used
+             * if (author.Id == id)
+             * {
+             *     context.Entry(author).State = EntityState.Modified;
+             *     context.SaveChanges();
+             *     return Ok();
+             * }
+             * else
+             * {
+             *     return BadRequest();
+             * }
+             */
+
+            List<Author> authors = GenerateAuthorsList(id + 5);
+            // authors.FirstOrDefault(authors => authors.Id == id) = author; NOT WORKING ???
+            authors.FirstOrDefault(authors => authors.Id == id).Name = author.Name;
+            authors.FirstOrDefault(authors => authors.Id == id).OriginCountry = author.OriginCountry;
+            authors.FirstOrDefault(authors => authors.Id == id).Birthdate = author.Birthdate;
+            return Ok($"Author updated: {authors.FirstOrDefault(authors => authors.Id == id).Name}");
         }
 
         // DELETE api/<AuthorController>/5
         [HttpDelete("{id}")]
         public IActionResult Delete(uint id)
         {
-            var author = context.Authors.FirstOrDefault(_author => _author.Id == id);
-            if (author != null)
-            {
-                context.Authors.Remove(author);
-                context.SaveChanges();
-                return Ok();
-            }
-            else
-            {
-                return BadRequest();
-            }
+            /* When DB is used...
+             * var author = context.Authors.FirstOrDefault(_author => _author.Id == id);
+             * if (author != null)
+             * {
+             *     context.Authors.Remove(author);
+             *     context.SaveChanges();
+             *     return Ok();
+             * }
+             * else
+             * {
+             *     return BadRequest();
+             * }
+             */
+
+            List<Author> authors = GenerateAuthorsList(id + 3);
+            var authorsNameDeleted = authors.FirstOrDefault(authors => authors.Id == id).Name;
+            authors.RemoveAt(authors.IndexOf(authors.FirstOrDefault(authors => authors.Id == id)));
+            return Ok($"Author {authorsNameDeleted} is now deleted");
         }
         #endregion
     }
